6fe6337dc41eac4a916824c3eb6bcdfbeada6946
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-12-15 17:12:49 -0800

    Initial commit for sub

---
+++ m_sub.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * m_sub - function that subtracts the top two elements of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: where the line number appears
+ * Description: 6. sub
+ * Return: on success nothing,on fail EXIT_FAILURE
+ */
+void m_sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first, *second;
+
+	if ((*stack == NULL) || ((*stack)->next == NULL))
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		fclose(file);
+		m_free(*stack);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		first = *stack;
+		second = first->next;
+		second->n -= first->n;
+		*stack = second;
+		(*stack)->prev = NULL;
+		free(first);
+	}
+}
