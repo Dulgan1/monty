d0af0286aab922625843369a6dc31dfa619bfaf7
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-12-15 17:09:22 -0800

    Initial commit

---
+++ m_rotr.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * mp_rotr - Rotate the right
+ * @stack: Stack
+ * @line_number: Line number
+ * Return: Void
+ */
+void m_rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first, *last;
+
+	(void)line_number;
+
+	if ((*stack == NULL) || ((*stack)->next == NULL))
+	{
+		;
+	}
+	else
+	{
+		first = last = *stack;
+		while (last->next)
+		{
+			last = last->next;
+		}
+		last->prev->next = NULL;
+		last->prev = NULL;
+		last->next = first;
+		first->prev = last;
+		*stack = last;
+	}
+}
