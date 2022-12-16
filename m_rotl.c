9c541252438780ea6a45eb3d4cc81953528d9133
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-12-15 17:06:40 -0800

    Initial commit for rotl

---
+++ m_rotl.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * m_rotl - function that rotates the tail to the top
+ * @stack: pointer to the top of the stack
+ * @line_number: where the line number appears
+ * Description: 13. rotl
+ * Return: on success nothing, on fail, EXIT_FAILURE
+ */
+
+void m_rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first, *last;
+
+	(void)line_number;
+
+	if ((*stack == NULL) || ((*stack)->next == NULL))
+	{
+		;
+	}
+	else
+	{
+		first = last = *stack;
+		while (last->next)
+		{
+			last = last->next;
+		}
+		*stack = first->next;
+		last->next = first;
+		first->prev = last;
+		first->next = NULL;
+		(*stack)->prev = NULL;
+	}
+}
